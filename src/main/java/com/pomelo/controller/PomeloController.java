41f23e01904b46f66c82aca19f6234353b8f4118
lihao <maigeyouzichi@gmail.com>
2023-03-24 14:42:15 +0800

    add WxPay notify api

--- src/main/java/com/pomelo/controller/PomeloController.java
+++ src/main/java/com/pomelo/controller/PomeloController.java
@@ -1,6 +1,8 @@
 package com.pomelo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/pomelo")
+@Slf4j
 public class PomeloController {
 
     @GetMapping
     public String get() {
         return "coding me";
+    }
+
+    @PostMapping("/notify/scanpay")
+    public String parseScanPayNotifyResult(String xmlData) {
+        log.info("微信支付回调数据:{}", xmlData);
+        return xmlData;
     }
 }
